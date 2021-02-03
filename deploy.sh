32526679c6254fabe25e1f69ce69c8d6cde280ea
eckhardt <eckhardt@silpion.de>
2021-02-03 19:30:33 +0100

    multiple jobs

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "starting deployment..."
+sleep 5
+echo "destroying production system..."
+sleep 4
+echo "sending out emails to customers..."
+sleep 3
+echo "going to file under chapter 11..."
+sleep 2
+echo "done. you may now go to bed."
+sleep 1
+
